9d04a36826da44f204f1220502e2bd1b2130bb93
Lucas Debeve <lucas.debeve2003@gmail.com>
2024-01-14 20:08:31 +0100

    Deploy 5

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 const devMode = process.env.NODE_ENV !== "production";
@@ -16,15 +14,12 @@ module.exports = {
             template: path.resolve(__dirname, "./src/template.html"),
             filename: "index.html",
         }),
-    ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+    ],
     module: {
         rules: [
             {
-                test: /\.(sa|sc|c)ss$/i,
-                use: [
-                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-                    "css-loader",
-                ],
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
@@ -33,7 +28,7 @@ module.exports = {
         ],
     },
     optimization: {
-        minimizer: [`...`, new TerserPlugin(), new CssMinimizerPlugin()],
+        minimizer: [`...`, new TerserPlugin()],
     },
     output: {
         filename: "bundle.js",
